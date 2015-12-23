71f7aec82b2c3066a7411197e4697f57608a40e5
kinokuniya <t13033ka@sfc.keio.ac.jp>
2015-12-23 13:18:06 +0000

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render test: "hello, world!"
+    render text: "hello, world!"
   end
 end
