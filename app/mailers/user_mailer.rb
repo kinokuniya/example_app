1291a3d513e68b72a204b13448a375309b8f793c
kinokuniya <t13033ka@sfc.keio.ac.jp>
2015-12-27 18:39:59 +0000

    Fix mailer again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+  default parts_order: ["text/html", "text/enriched", "text/plain"]
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
